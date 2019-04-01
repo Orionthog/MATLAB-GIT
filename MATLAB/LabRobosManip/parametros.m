60e7ab334d28de208e0b4dfb3e470713cd920feb
Orionthog <38017504+Orionthog@users.noreply.github.com>
2019-04-01 17:57:48 -0300

    Add files via upload
    aaaaaaaaaaaaaaaaaaaaaaaah

--- MATLAB/LabRobosManip/parametros.m
+++ MATLAB/LabRobosManip/parametros.m
@@ -1,4 +1,4 @@
-function  [usrRef, usrRotx, usrRoty, usrRotz] = parametros()
+function  [usrRef, usrRotx, usrRoty, usrRotz] = parametros(P0)
 while(1)
     usrRef = input('Sistema de referencia fixo no mundo (W), ou fixo no cubo (C): \n', 's');
     while(usrRef ~= 'W' & usrRef ~= 'C')
@@ -6,9 +6,14 @@ while(1)
         usrRef = input('Sistema de referencia fixo no mundo (W), ou fixo no cubo (C): \n', 's');
     end
     %Valores em graus
-    usrRotx = trotx(input('Rotação em torno de X: '));
-    usrRoty = troty(input('Rotação em torno de Y: '));
-    usrRotz = trotz(input('Rotação em torno de Z: '));
+    usrRotx = input('Rotação em torno de X: ');
+    usrRoty = input('Rotação em torno de Y: ');
+    usrRotz = input('Rotação em torno de Z: ');
+    
+    %
+    DesenhaCubo(P0)
+    rotacao(usrRef, usrRotx, usrRoty, usrRotz)
+    DesenhaCubo(P0)
     
     while(1)
         usrAskAgain = input('Deseja fazer outra transformação? [S/N]\n', 's');
--- MATLAB/LabRobosManip/rotacao.m
+++ MATLAB/LabRobosManip/rotacao.m
@@ -1,23 +1,16 @@
-function H = rotacao(usrRef, usrRotx, usrRoty, usrRotz)
+%function [H, P0] = rotacao(usrRef, usrRotx, usrRoty, usrRotz)
+function P0 = rotacao(usrRef, usrRotx, usrRoty, usrRotz)
+    %{
     if (usrRef == 'W')
         H = (usrRotx*pi/180)*(usrRoty*pi/180)*(usrRotz*pi/180);
     elseif(usrRef == 'C')
         H = 
     else
     end
-    
-    figure(2)
-    hold on
-    axis equal;
-    grid on;
-    xlabel('X','FontSize',18);
-    ylabel('Y','FontSize',18);
-    zlabel('Z','FontSize',18);
-    xlim([-2, 2]);
-    ylim([-2, 2]);
-    zlim([-2, 2]);
-    
-    %P1 = H * [P(:,1); 1]
-
+    %}
+    P = [ [0; 0; 0], [0; 1; 0],  [1; 1; 0], [1; 0; 0], [0; 0; 1], [0; 1; 1], [1; 1; 1], [1; 0; 1]];
+    H = rotx(usrRotx*pi/180)*roty(usrRoty*pi/180)*rotz(usrRotz*pi/180)
+    P0 = [H*P(:,1), H*P(:,1), H*P(:,1), H*P(:,1), H*P(:,1), H*P(:,1), H*P(:,1), H*P(:,1)]
+    %P = [P0(:,1)*H, P0(:,2)*H, P0(:,3)*H, P0(:,4)*H, P0(:,5)*H, P0(:,6)*H, P0(:,7)*H, P0(:,8)*H]
     
 end
