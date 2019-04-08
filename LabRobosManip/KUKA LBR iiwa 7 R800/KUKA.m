a71a60e55a413b14586e162a2fa0e3dd5d82954c
Orionthog <38017504+Orionthog@users.noreply.github.com>
2019-04-08 17:13:21 -0300

    Add files via upload
    Para de travaaaaaaaar

--- LabRobosManip/KUKA LBR iiwa 7 R800/KUKA.m
+++ LabRobosManip/KUKA LBR iiwa 7 R800/KUKA.m
@@ -1,4 +1,10 @@
+%%
 
+%Parâmetros
+a1 = 340;
+a2 = 400;
+a3 = 400;
+a4 = 126;
 theta1 = 0;
 theta2 = 0;
 theta3 = 0;
@@ -6,13 +12,17 @@ theta4 = 0;
 theta5 = 0;
 theta6 = 0;
 theta7 = 0;
-
-L(1) = Link([0, 3*pi/2, 0.0, pi/2 + theta1, 0]);
-L(2) = Link([0, 1*pi/2, 0.0, pi/2 + theta2, 0]);
-L(3) = Link([0, 1*pi/2, 0.0, pi/2 + theta3, 0]);
-L(4) = Link([0, 3*pi/2, 0.0, pi/2 + theta4, 0]);
-L(5) = Link([0, 3*pi/2, 0.0, pi/2 + theta5, 0]);
-L(6) = Link([0, 1*pi/2, 0.0, pi/2 + theta6, 0]);
-L(7) = Link([0,   0   , 0.0, pi/2 + theta7, 0]);
+%Braço
+L(1) = Link([0, -1*pi/2,  a1 , -pi/2 + theta1, 0]);
+L(2) = Link([0,  1*pi/2,  0.0,    0  + theta2, 0]);
+L(3) = Link([0,  1*pi/2, a2/2,    0  + theta3, 0]);
+L(4) = Link([0, -1*pi/2,  0.0,    0  + theta4, 0]);
+L(5) = Link([0, -1*pi/2, a3/2,    0  + theta5, 0]);
+L(6) = Link([0,  1*pi/2,  0.0,    0  + theta6, 0]);
+L(7) = Link([0,    0   , a4/2,  pi/2 + theta7, 0]);
  
-twolinks = SerialLink(L, 'name', 'two link') 
+kuka = SerialLink(L, 'name', 'kuka') 
+
+%%
+%Plot
+kuka.plot([0 0 0 0 0 0 0])
