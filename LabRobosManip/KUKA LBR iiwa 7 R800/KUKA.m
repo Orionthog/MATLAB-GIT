55662c6fdb90c70370778a381fad16f72fa75b83
orionthog <dreskenbanner@gmail.com>
2019-04-22 17:38:32 -0300

    Pray for RAM

--- LabRobosManip/KUKA LBR iiwa 7 R800/KUKA.m
+++ LabRobosManip/KUKA LBR iiwa 7 R800/KUKA.m
@@ -1,6 +1,7 @@
 %%
-
-%Parâmetros
+run('/home/igor.barbara/github/MATLAB-GIT/LabRobosManip/rvctools/startup_rvc.m')
+%%
+%Parametros
 a1 = 340;
 a2 = 400;
 a3 = 400;
@@ -12,17 +13,18 @@ theta4 = 0;
 theta5 = 0;
 theta6 = 0;
 theta7 = 0;
-%Braço
-L(1) = Link([0, -1*pi/2,  a1 , -pi/2 + theta1, 0]);
-L(2) = Link([0,  1*pi/2,  0.0,    0  + theta2, 0]);
-L(3) = Link([0,  1*pi/2, a2/2,    0  + theta3, 0]);
-L(4) = Link([0, -1*pi/2,  0.0,    0  + theta4, 0]);
-L(5) = Link([0, -1*pi/2, a3/2,    0  + theta5, 0]);
-L(6) = Link([0,  1*pi/2,  0.0,    0  + theta6, 0]);
-L(7) = Link([0,    0   , a4/2,  pi/2 + theta7, 0]);
- 
+%Braco
+L(1) = Link([ -pi/2 + theta1 ,  a1  , 0 , -1*pi/2]);
+L(2) = Link([   0   + theta2 ,  0   , 0 ,  1*pi/2]);
+L(3) = Link([   0   + theta3 , a2/2 , 0 ,  1*pi/2]);
+L(4) = Link([   0   + theta4 ,  0   , 0 , -1*pi/2]);
+L(5) = Link([   0   + theta5 , a3/2 , 0 , -1*pi/2]);
+%L(6) = Link([   0   + theta6 ,  0   , 0 , -1*pi/2]);
+%L(7) = Link([  pi/2 + theta7 , a4/2 , 0 ,     0  ]);
+
 kuka = SerialLink(L, 'name', 'kuka') 
 
 %%
 %Plot
-kuka.plot([0 0 0 0 0 0 0])
+figure(1)
+kuka.plot(zeros(1, kuka.n))
